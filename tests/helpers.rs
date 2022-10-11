8553f413f4338c4d7187a48653f92bacbf273983
Justus Adam <dev@justus.science>
2022-10-10 22:06:27 -0400

    Added more non-transitive graph tests
    Copied test cases out of dfpp-examples by @livia01px2019

---
+++ tests/helpers.rs
@@ -0,0 +1,83 @@
+#![feature(rustc_private)] 
+extern crate rustc_middle;
+extern crate rustc_span;
+use dfpp::Symbol;
+use rustc_middle::mir;
+
+pub fn with_current_directory<
+    P: AsRef<std::path::Path>,
+    A,
+    F: std::panic::UnwindSafe + FnOnce() -> A,
+>(
+    p: P,
+    f: F,
+) -> std::io::Result<A> {
+    let current = std::env::current_dir()?;
+    std::env::set_current_dir(p)?;
+    let res = std::panic::catch_unwind(f);
+    std::env::set_current_dir(current)?;
+    match res {
+        Ok(r) => Ok(r),
+        Err(e) => std::panic::resume_unwind(e),
+    }
+}
+
+pub fn cwd_and_use_rustc_in<P: AsRef<std::path::Path>, A, F: std::panic::UnwindSafe + FnOnce() -> A>(
+    p: P,
+    f: F,
+) -> std::io::Result<A> {
+    with_current_directory(p, || {
+        rustc_span::create_default_session_if_not_set_then(|_| f())
+    })
+}
+
+pub fn run_dfpp_with_graph_dump() -> bool {
+    std::process::Command::new("cargo")
+        .arg("dfpp")
+        .arg("--dump-serialized-non-transitive-graph")
+        .status()
+        .unwrap()
+        .success()
+}
+
+pub type SimpleMirBody = Vec<(mir::Location, String)>;
+
+use dfpp::foreign_serializers::SerializableNonTransitiveGraph;
+
+pub struct G {
+    pub graph: SerializableNonTransitiveGraph,
+    pub body: SimpleMirBody,
+}
+
+impl G {
+    pub fn connects(&self, from: mir::Location, to: mir::Location) -> bool {
+        let mut queue = vec![to];
+        while let Some(n) = queue.pop() {
+            if n == from {
+                return true;
+            }
+            self.graph[&n]
+                .rows()
+                .map(|p| p.1)
+                .for_each(|s| queue.extend(s.iter()))
+        }
+        false
+    }
+    pub fn connects_direct(&self, from: mir::Location, to: mir::Location) -> bool {
+        self.graph[&to]
+            .rows()
+            .any(|(_, r)| r.iter().any(|i| i == &from))
+    }
+
+    pub fn function_call(&self, pattern: &str) -> mir::Location {
+        self.body
+            .iter()
+            .find(|(_, s)| s.contains(pattern))
+            .unwrap_or_else(|| panic!("Pattern {pattern} not found in {:?}", self.body))
+            .0
+    }
+    pub fn from_file(s: Symbol) -> Self {
+        let (body, graph) = dfpp::dbg::read_non_transitive_graph_and_body(s);
+        Self { graph, body }
+    }
+}
---
+++ tests/non_transitive_graph_tests.rs
@@ -0,0 +1,94 @@
+#![feature(rustc_private)]
+#[macro_use]
+extern crate lazy_static;
+
+use dfpp::Symbol;
+mod helpers;
+use helpers::*;
+
+fn do_in_crate_dir<A, F: std::panic::UnwindSafe + FnOnce() -> A>(f: F) -> std::io::Result<A> {
+    let crate_dir : std::path::PathBuf = "tests/non-transitive-graph-tests".to_string().into();
+    cwd_and_use_rustc_in(
+        &crate_dir,
+        f
+    )
+}
+
+lazy_static! {
+    static ref TEST_CRATE_ANALYZED: bool = do_in_crate_dir(
+        || {
+            run_dfpp_with_graph_dump()
+        }
+    ).map_or_else(|e| { println!("io err {}", e); false }, |t| t);
+}
+
+
+#[test]
+fn simple_happens_before_has_connections() {
+    assert!(*TEST_CRATE_ANALYZED);
+    let graph = do_in_crate_dir(|| {
+        G::from_file(Symbol::intern("basic_happens_before"))
+    })
+    .unwrap();
+
+    let get = graph.function_call("get_user_data");
+    let dp = graph.function_call("dp_user_data");
+    let send = graph.function_call("send_user_data");
+
+    assert!(graph.connects(get, dp));
+    assert!(graph.connects(dp, send));
+    assert!(graph.connects(get, send));
+    assert!(!graph.connects_direct(get, send))
+}
+
+#[test]
+fn happens_before_if_has_connections() {
+    assert!(*TEST_CRATE_ANALYZED);
+    let graph = do_in_crate_dir(|| {
+        G::from_file(Symbol::intern("conditional_happens_before"))
+    })
+    .unwrap();
+
+    let get = graph.function_call("get_user_data");
+    let dp = graph.function_call("dp_user_data");
+    let send = graph.function_call("send_user_data");
+    assert!(graph.connects(get, dp,));
+    assert!(graph.connects(dp, send));
+    assert!(graph.connects_direct(get, send));
+}
+
+#[test]
+fn data_influenced_conditional_happens_before() {
+    assert!(*TEST_CRATE_ANALYZED);
+    let graph = do_in_crate_dir(|| {
+        G::from_file(Symbol::intern("data_influenced_conditional_happens_before"))
+    })
+    .unwrap();
+
+    let get = graph.function_call("get_user_data");
+    let dp = graph.function_call("dp_user_data");
+    let send = graph.function_call("send_user_data");
+    assert!(graph.connects(get, dp,));
+    assert!(graph.connects(dp, send));
+    assert!(graph.connects_direct(get, send));
+}
+
+
+#[test]
+fn conditional_happens_before_with_two_parents_before_if() {
+    assert!(*TEST_CRATE_ANALYZED);
+    let graph = do_in_crate_dir(|| {
+        G::from_file(Symbol::intern("conditional_happens_before_with_two_parents_before_if"))
+    })
+    .unwrap();
+
+    let get = graph.function_call("get_user_data_with");
+    let dp = graph.function_call("dp_user_data");
+    let send = graph.function_call("send_user_data");
+    let push = graph.function_call("push");
+    assert!(graph.connects(push, get));
+    assert!(graph.connects(get, dp));
+    assert!(graph.connects(dp, send));
+    assert!(graph.connects_direct(get, send));
+    assert!(!graph.connects_direct(push, send));
+}
