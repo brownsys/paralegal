19e2412562583a29b8cf73a8ecb7a37dbf180613
Justus Adam <dev@justus.science>
2022-10-10 22:06:27 -0400

    Added more non-transitive graph tests
    Copied test cases out of dfpp-examples by @livia01px2019

---
+++ tests/non-transitive-graph-tests/src/main.rs
@@ -0,0 +1,71 @@
+#![feature(register_tool)]
+#![register_tool(dfpp)]
+
+#[dfpp::label(sensitive)]
+struct UserData {
+    pub data: Vec<i64>,
+}
+
+#[dfpp::analyze]
+fn basic_happens_before() {
+    let mut user_data = get_user_data();
+    dp_user_data(&mut user_data);
+    send_user_data(&user_data);
+}
+
+#[dfpp::analyze]
+fn conditional_happens_before(cond: bool) {
+    let mut user_data = get_user_data();
+    if cond {
+        dp_user_data(&mut user_data);
+    }
+    send_user_data(&user_data);
+}
+
+fn data_contains_3(d: &UserData) -> bool {
+    d.data.iter().any(|i| i == 3)
+}
+
+#[dfpp::analyze]
+fn data_influenced_conditional_happens_before() {
+    let mut user_data = get_user_data();
+    if data_contains_3(&user_data) {
+        dp_user_data(&mut user_data);
+    }
+    send_user_data(&user_data);
+}
+
+#[dfpp::analyze]
+fn conditional_happens_before_with_two_parents_before_if(mut d: Vec<i64>, cond: bool) {
+    d.push(6);
+    let mut user_data = get_user_data_with(d);
+    if cond {
+        dp_user_data(&mut user_data);
+    }
+    send_user_data(&user_data);
+
+}
+
+#[dfpp::label(source)]
+fn get_user_data() -> UserData {
+    return UserData{data: vec![1, 2, 3]}
+}
+
+#[dfpp::label(source)]
+fn get_user_data_with(data: Vec<i64>) -> UserData {
+    return UserData{data}
+}
+
+fn dp_user_data(user_data: &mut UserData) {
+    for i in &mut user_data.data {
+        *i = 2;
+    }
+}
+
+#[dfpp::label{ sink, arguments = [0] }]
+fn send_user_data(user_data: &UserData) {
+}
+
+fn main() {
+    println!("Hello, world!");
+}
