5293624e32a1a40f9cd49d8f288861dc857b74dc
livia01px2019 <livia.zhu@live.com>
2023-01-09 14:58:41 -0800

    added blessed safe source tests and start of conditional modification

--- tests/forge-tests/src/main.rs
+++ tests/forge-tests/src/main.rs
@@ -40,4 +40,84 @@ fn check_user_data(user_data: &UserData) -> bool {
 fn send_user_data(_user_data: &UserData) {
 }
 
+#[dfpp::analyze]
+fn blessed_safe_sources(config: u8) {
+	let mut recipients = if get_num() < 90 {
+		get_staff(config)
+	} else {
+		get_admins(config)
+	};
+	send(recipients)
+}
+
+#[dfpp::analyze]
+fn only_safe_sources(config: u8) {
+	let mut recipients = get_admins(config);
+	send(recipients)
+}
+
+#[dfpp::analyze]
+fn unblessed_safe_sources_with_bless(config: u8) {
+	let mut recipients = get_staff(config);
+	send(recipients)
+}
+
+#[dfpp::analyze]
+fn unsafe_sources(config: u8) {
+	let mut recipients = if get_num() < 90 {
+		get_staff(config)
+	} else {
+		get_admins(config)
+	};
+	let mut evil = vec!["evil@evil.com".to_string()];
+	recipients.append(&mut evil);
+	send(recipients)
+}
+
+#[dfpp::analyze]
+fn blessed_and_unblessed_safe_sources(config: u8) {
+	let mut recipients = if get_num() < 90 {
+		get_staff(config)
+	} else {
+		get_admins(config)
+	};
+	recipients.append(&mut get_staff(config));
+	send(recipients)
+}
+
+// The following fails only_send_to_allowed_sources because bless flows into recipients after the instantiation of safe_source_with_bless. This cannot be permitted because it looks identical in the graph as if we added staff and then did the check later in some irrelevant place. I think we do need to have some concept of the speciality of modifying fns like append, push, etc. 
+#[dfpp::analyze]
+fn conditional_modification(config: u8) {
+	let mut recipients = empty_vec();
+	let mut staff = get_staff(config);
+	if get_num() < 90 {
+		recipients.append(&mut staff);
+	}
+	send(recipients);
+}
+
+#[dfpp::label(safe_source, return)]
+fn empty_vec() -> Vec<String> {
+	vec![]
+}
+
+#[dfpp::label(bless_safe_source, return)]
+fn get_num() -> u8 {
+	10
+}
+
+#[dfpp::label(safe_source_with_bless, return)]
+fn get_staff(config: u8) -> Vec<String> {
+	vec![]
+}
+
+#[dfpp::label(safe_source, return)]
+fn get_admins(config: u8) -> Vec<String> {
+	vec![]
+}
+
+#[dfpp::label{ scopes, arguments = [0] }]
+fn send(recipients: Vec<String>) {
+}
+
 fn main() {}
