d56ebad103d70419dcaaf4cf3bedf7f54fd84431
Justus Adam <dev@justus.science>
2023-10-24 10:25:30 -0700

    Remove Laziness for Markers (#89)
    ## What Changed?
    
    Eagerly visit all IDs in the program under analysis and register
    potential markers.
    Rips out all the infrastructure that was used to do it lazily.
    
    Refactors the `CollectingVisitor` into two structs, the visitor and then
    an `SPDGGenerator` that runs the actual analysis. This separates the
    concerns out better. The former stores (among other things) a mutable
    `MarkerDatabase` that it fills with the markers for local functions and
    types and it collects a vector of analysis targets.
    Once the visit is done it is converted into the `SPDGGenerator` which
    stores the read-only `MarkerCtx`.
    
    ## Why Does It Need To?
    
    Lazy markers were confusing. Markers on controllers were never
    registered. Also could cause surprising vacuity if markers were never
    encountered because e.g. their call sites happened in a closure.
    
    ## Checklist
    
    - [x] Above description has been filled out so that upon quash merge we
    have a
      good record of what changed.
    - [x] New functions, methods, types are documented. Old documentation is
    updated
      if necessary
    - [x] Documentation in Notion has been updated
      - [x] Remove from list of arguments
      - [x] Remove from step-by-step guide
    - [x] Tests for new behaviors are provided
      - [x] New test suites (if any) ave been added to the CI tests (in
    `.github/workflows/rust.yml`) either as compiler test or integration
    test.
    *Or* justification for their omission from CI has been provided in this
    PR
        description.

--- crates/paralegal-flow/src/ana/mod.rs
+++ crates/paralegal-flow/src/ana/mod.rs
@@ -1,11 +1,14 @@
 //! Data-and control flow analyzer and inliner.
 //!
-//!
+//! Analysis starts with the construction of [`SPDGGenerator`] from a
+//! [`CollectingVisitor`](crate::discover::CollectingVisitor) and then calling
+//! [`analyze`](SPDGGenerator::analyze).
 
 use std::borrow::Cow;
 
 use crate::{
-    dbg, desc::*, ir::*, rust::*, utils::*, Either, HashMap, HashSet, LogLevelConfig, Symbol,
+    dbg, desc::*, ir::*, rust::*, utils::*, Either, HashMap, HashSet, LogLevelConfig, MarkerCtx,
+    Symbol,
 };
 
 use hir::def_id::DefId;
@@ -13,7 +16,7 @@ use mir::Location;
 
 use anyhow::Result;
 
-use super::discover::{CallSiteAnnotations, CollectingVisitor, FnToAnalyze};
+use super::discover::{CallSiteAnnotations, FnToAnalyze};
 
 pub mod algebra;
 pub mod df;
@@ -23,15 +26,22 @@ pub mod non_transitive_aliases;
 pub type SerializableInlinedGraph<L> =
     petgraph::graphmap::GraphMap<regal::SimpleLocation<L>, inline::Edge, petgraph::Directed>;
 
-impl<'tcx> CollectingVisitor<'tcx> {
-    /// Driver function. Performs the data collection via visit, then calls
-    /// [`Self::analyze`] to construct the Forge friendly description of all
-    /// endpoints.
-    pub fn run(mut self) -> Result<ProgramDescription> {
-        let tcx = self.tcx;
-        tcx.hir().visit_all_item_likes_in_crate(&mut self);
-        //println!("{:?}\n{:?}\n{:?}", self.marked_sinks, self.marked_sources, self.functions_to_analyze);
-        self.analyze()
+/// Read-only database of information the analysis needs.
+///
+/// [`Self::analyze`] serves as the main entrypoint to SPDG generation.
+pub struct SPDGGenerator<'tcx> {
+    pub marker_ctx: MarkerCtx<'tcx>,
+    pub opts: &'static crate::Args,
+    pub tcx: TyCtxt<'tcx>,
+}
+
+impl<'tcx> SPDGGenerator<'tcx> {
+    pub fn new(marker_ctx: MarkerCtx<'tcx>, opts: &'static crate::Args, tcx: TyCtxt<'tcx>) -> Self {
+        Self {
+            marker_ctx,
+            opts,
+            tcx,
+        }
     }
 
     /// Perform the analysis for one `#[paralegal_flow::analyze]` annotated function and
@@ -40,7 +50,7 @@ impl<'tcx> CollectingVisitor<'tcx> {
         &self,
         //_hash_verifications: &mut HashVerifications,
         call_site_annotations: &mut CallSiteAnnotations,
-        target: FnToAnalyze,
+        target: &FnToAnalyze,
         inliner: &inline::Inliner<'tcx>,
     ) -> anyhow::Result<(Endpoint, Ctrl)> {
         let mut flows = Ctrl::default();
@@ -241,9 +251,7 @@ impl<'tcx> CollectingVisitor<'tcx> {
     /// other setup necessary for the flow graph creation.
     ///
     /// Should only be called after the visit.
-    fn analyze(mut self) -> Result<ProgramDescription> {
-        let mut targets = std::mem::take(&mut self.functions_to_analyze);
-
+    pub fn analyze(&self, targets: &[FnToAnalyze]) -> Result<ProgramDescription> {
         if let LogLevelConfig::Targeted(s) = self.opts.debug() {
             assert!(
                 targets.iter().any(|target| target.name().as_str() == s),
@@ -266,7 +274,7 @@ impl<'tcx> CollectingVisitor<'tcx> {
         let mut call_site_annotations: CallSiteAnnotations = HashMap::new();
         let result = //crate::sah::HashVerifications::with(|hash_verifications| {
             targets
-                .drain(..)
+                .iter()
                 .map(|desc| {
                     let target_name = desc.name();
                     with_reset_level_if_target(self.opts, target_name, || {
