869b02ca4a7a006672f0683b630bfde523bc18c6
Justus Adam <justbldr@amazon.com>
2023-10-30 14:27:07 -0700

    Caller-independent scripts

--- scripts/integration-tests.sh
+++ scripts/integration-tests.sh
@@ -1,7 +1,12 @@
 # Before editing this script ensure you have read and understood the
-# relationship of the scripts with one another and with the CI as alid out in
+# relationship of the scripts with one another and with the CI as laid out in
 # the README.md in this directory.
 
 set -x
+
+SCRIPT_DIR="$( cd "$( dirname "$0" )" &> /dev/null && pwd )"
+
+# cd into root directory of the repo
+cd $SCRIPT_DIR/..
 
 cargo test -p paralegal-flow --test external_annotation_tests
