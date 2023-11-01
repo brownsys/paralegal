b9cf2ed6628e38c695eb065e518f405bdc37faa9
Livia Zhu <37862641+livia01px2019@users.noreply.github.com>
2023-11-01 18:36:09 -0400

    Implement Websubmit authorized disclosure property (#103)
    ## What Changed?
    
    Add authorized disclosure property to Websubmit's props.
    
    *Note:*
    
    Passing authorized disclosure property required commenting out the
    `scopes` marker in on `replace` in backend.rs in beavered_websubmit
    ([link to
    commit](https://github.com/JustusAdam/beavered-websubmit/commit/11f116d65aee00523a26754a1685eb5c61756021)).
    
    In the Forge property, we defined scopes to be arguments of the callsite
    that are marked with scopes. In the Rust property, we define scopes to
    be anything that flows into the callsite that is marked with scopes,
    which is in line with how scoped_storage does it.
    
    The Rust property fails with this marker commented back in because the
    scoped argument to replace flows into the email::my_send function via
    the bg overtaint, and it thinks that anything that flows into this
    argument is a receiver for the email, which is incorrect.
    
    ## Why Does It Need To?
    
    Last Websubmit property yay!
    
    ## Checklist
    
    - [x] Above description has been filled out so that upon quash merge we
    have a
      good record of what changed.
    - [ ] New functions, methods, types are documented. Old documentation is
    updated
      if necessary
    - [ ] Documentation in Notion has been updated
    - [ ] Tests for new behaviors are provided
      - [ ] New test suites (if any) ave been added to the CI tests (in
    `.github/workflows/rust.yml`) either as compiler test or integration
    test.
    *Or* justification for their omission from CI has been provided in this
    PR
        description.
    
    ---------
    
    Co-authored-by: Justus Adam <justbldr@amazon.com>

--- props/websubmit/src/main.rs
+++ props/websubmit/src/main.rs
@@ -99,7 +99,7 @@ impl ScopedStorageProp {
             let scopes = self
                 .cx
                 .all_nodes_for_ctrl(*c_id)
-                .filter(|node| self.cx.has_marker(marker!(scopes), *node))
+                .filter(|node| self.cx.has_marker(marker!(scopes_store), *node))
                 .collect::<Vec<_>>();
             let stores = self
                 .cx
@@ -160,6 +160,116 @@ fn run_sc_policy(ctx: Arc<Context>) -> Result<()> {
     })
 }
 
+/// If sensitive data is released, the release must be scoped, and all inputs to the scope must be safe.
+pub struct AuthDisclosureProp {
+    cx: Arc<PolicyContext>,
+}
+
+impl AuthDisclosureProp {
+    pub fn new(cx: Arc<PolicyContext>) -> Self {
+        AuthDisclosureProp { cx }
+    }
+
+    pub fn check(self) -> Result<()> {
+        for c_id in self.cx.desc().controllers.keys() {
+            // All srcs that have no influencers
+            let roots = self
+                .cx
+                .roots(*c_id, paralegal_policy::EdgeType::DataAndControl)
+                .collect::<Vec<_>>();
+
+            let safe_scopes = self
+                .cx
+                // All nodes marked "safe"
+                .all_nodes_for_ctrl(*c_id)
+                .filter(|n| self.cx.has_marker(marker!(safe_source), *n))
+                // And all nodes marked "safe_with_bless"
+                .chain(self.cx.all_nodes_for_ctrl(*c_id).filter(|node| {
+                    self.cx.has_marker(marker!(safe_source_with_bless), *node)
+                        && self
+                            .cx
+                            // That are influenced by a node marked "bless"
+                            .influencers(*node, paralegal_policy::EdgeType::DataAndControl)
+                            .any(|b| self.cx.has_marker(marker!(bless_safe_source), b))
+                }))
+                .collect::<Vec<_>>();
+            let sinks = self
+                .cx
+                .all_nodes_for_ctrl(*c_id)
+                .filter(|n| self.cx.has_marker(marker!(sink), *n))
+                .collect::<Vec<_>>();
+            let sensitives = self
+                .cx
+                .all_nodes_for_ctrl(*c_id)
+                .filter(|node| self.cx.has_marker(marker!(sensitive), *node));
+
+            for sens in sensitives {
+                for sink in sinks.iter() {
+                    // sensitive flows to store implies
+                    if !self
+                        .cx
+                        .flows_to(sens, *sink, paralegal_policy::EdgeType::Data)
+                    {
+                        continue;
+                    }
+
+                    let Some(sink_callsite) = sink.associated_call_site() else {
+                        assert_error!(
+                            self.cx,
+                            false,
+                            format!(
+                                "sink {} does not have associated callsite",
+                                self.cx.describe_node(*sink)
+                            )
+                        );
+                        continue;
+                    };
+
+                    // scopes for the store
+                    let store_scopes = self
+                        .cx
+                        .influencers(sink_callsite, paralegal_policy::EdgeType::Data)
+                        .filter(|n| self.cx.has_marker(marker!(scopes), *n))
+                        .collect::<Vec<_>>();
+                    assert_error!(
+                        self.cx,
+                        !store_scopes.is_empty(),
+                        format!(
+                            "Did not find any scopes for sink {}",
+                            self.cx.describe_node(*sink)
+                        )
+                    );
+
+                    // all flows are safe before scope
+                    let safe_before_scope = self.cx.always_happens_before(
+                        roots.iter().cloned(),
+                        |n| safe_scopes.contains(&n),
+                        |n| store_scopes.contains(&n),
+                    )?;
+
+                    assert_error!(
+                        self.cx,
+                        safe_before_scope.holds(),
+                        format!(
+                            "Sensitive {} flowed to sink {} which did not have safe scopes",
+                            self.cx.describe_node(sens),
+                            self.cx.describe_node(*sink),
+                        )
+                    );
+                    safe_before_scope.report(self.cx.clone());
+                }
+            }
+        }
+        Ok(())
+    }
+}
+
+fn run_dis_policy(ctx: Arc<Context>) -> Result<()> {
+    ctx.named_policy(Identifier::new_intern("Authorized Disclosure"), |ctx| {
+        AuthDisclosureProp::new(ctx).check()
+    })
+}
+
 fn main() -> Result<()> {
     let ws_dir = std::env::args()
         .nth(1)
@@ -170,9 +280,12 @@ fn main() -> Result<()> {
         Some(s) => match s.as_str() {
             "sc" => run_sc_policy,
             "del" => run_del_policy,
+            "dis" => run_dis_policy,
             other => bail!("don't recognize the property name '{other}'"),
         },
-        None => |ctx: Arc<Context>| run_sc_policy(ctx.clone()).and(run_del_policy(ctx)),
+        None => |ctx: Arc<Context>| {
+            run_dis_policy(ctx.clone()).and(run_sc_policy(ctx.clone()).and(run_del_policy(ctx)))
+        },
     };
 
     paralegal_policy::SPDGGenCommand::global()
