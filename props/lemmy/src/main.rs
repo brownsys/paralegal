5dc195ff992001ae429f5ba38a97eb6d011e4af4
Justus Adam <justbldr@amazon.com>
2024-02-27 18:52:19 +0000

    Updating lemmy

--- props/lemmy/src/main.rs
+++ props/lemmy/src/main.rs
@@ -1,12 +1,16 @@
 extern crate anyhow;
 
 use anyhow::{anyhow, Result};
+use clap::Parser;
+use std::io::stdout;
+use std::path::PathBuf;
 use std::sync::Arc;
+use std::time::{Duration, Instant};
 
 use paralegal_policy::{
     assert_error,
     paralegal_spdg::{traverse::EdgeSelection, GlobalNode, Identifier},
-    Marker, PolicyContext,
+    Marker, PolicyContext, Context
 };
 
 pub struct CommunityProp {
@@ -28,7 +32,7 @@ impl CommunityProp {
     }
 
     pub fn check(&mut self) {
-        let db_community_write = Marker::new_intern("db_community_write");
+        let db_community_write = Marker::new_intern("db_access");
         let community_delete_check = Marker::new_intern("community_delete_check");
         let community_ban_check = Marker::new_intern("community_ban_check");
 
@@ -46,16 +50,46 @@ impl CommunityProp {
     }
 }
 
-fn main() -> Result<()> {
-    let lemmy_dir = std::env::args()
-        .nth(1)
-        .ok_or_else(|| anyhow!("expected an argument"))?;
-    paralegal_policy::SPDGGenCommand::global()
-        .run(lemmy_dir)?
-        .with_context(|ctx| {
-            ctx.named_policy(Identifier::new_intern("Community Policy"), |ctx| {
-                CommunityProp::new(ctx).check();
-                Ok(())
-            })
-        })
+#[derive(Parser)]
+struct Arguments {
+    path: PathBuf,
+    #[clap(last = true)]
+    extra_args: Vec<String>,
+}
+
+fn time<T>(f: impl FnOnce() -> T) -> (T, Duration) {
+    let now = Instant::now();
+    let result = f();
+    let elapsed = now.elapsed();
+    (result, elapsed)
+}
+
+fn main() -> anyhow::Result<()> {
+    let args: &'static Arguments = Box::leak(Box::new(Arguments::parse()));
+
+    let mut cmd = paralegal_policy::SPDGGenCommand::global();
+    cmd.external_annotations("external-annotations.toml");
+    cmd.abort_after_analysis();
+    cmd.get_command().arg("--target").arg("lemmy_api");
+    cmd.get_command().args(&args.extra_args);
+
+    let (graph, compile_time) = time(|| cmd.run(&args.path));
+
+    let (res, policy_time) = time(|| {
+        let ctx = Arc::new(graph?.build_context()?);
+        let num_controllers = ctx.desc().controllers.len();
+        let sum_nodes = ctx.desc().controllers.values().map(|spdg| spdg.graph.node_count()).sum::<usize>();
+        println!("Analyzing over {num_controllers} controllers with avg {} nodes per graph", sum_nodes / num_controllers);
+        ctx.clone().named_policy(Identifier::new_intern("Community Policy"), |ctx| {
+            CommunityProp::new(ctx.clone()).check()
+        });
+        anyhow::Ok(ctx)
+    });
+    println!(
+        "Policy finished. Analysis took {}, policy took {}",
+        humantime::Duration::from(compile_time),
+        humantime::Duration::from(policy_time)
+    );
+    res?.emit_diagnostics_may_exit(stdout())?;
+    anyhow::Ok(())
 }
