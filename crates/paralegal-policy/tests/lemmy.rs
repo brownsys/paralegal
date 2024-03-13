8e0be6459acbfe81100afaf3ea9130c44d0dc196
Justus Adam <justbldr@amazon.com>
2024-03-13 13:34:59 -0400

    Calling async trait test case from lemmy

--- crates/paralegal-policy/tests/lemmy.rs
+++ crates/paralegal-policy/tests/lemmy.rs
@@ -59,7 +59,7 @@ fn async_trait_policy(ctx: Arc<Context>) -> Result<()> {
 
 /// Tests we can handle `async_trait` version 0.1.53
 #[test]
-fn async_trait_1_53() -> Result<()> {
+fn async_trait_0_1_53() -> Result<()> {
     let mut test = Test::new(ASYNC_TRAIT_CODE)?;
     test.with_dep(["async-trait@=0.1.53"]);
     test.run(async_trait_policy)
@@ -72,4 +72,55 @@ fn async_trait_latest() -> Result<()> {
     let mut test = Test::new(ASYNC_TRAIT_CODE)?;
     test.with_dep(["async-trait"]);
     test.run(async_trait_policy)
+}
+
+const CALLING_ASYNC_TRAIT_CODE: &str = stringify!(
+    struct Ctx(usize, bool);
+
+    #[paralegal::marker(source, return)]
+    async fn source(_context: &Ctx) -> usize {
+        0
+    }
+
+    #[paralegal::marker(sink, arguments = [0])]
+    fn sink<T>(sink: T) {}
+
+    #[paralegal::analyze]
+    async fn main() {
+        sink(Ctx(8, true).foo().await.unwrap())
+    }
+
+    #[async_trait::async_trait(?Send)]
+    trait AsyncTrait {
+        async fn foo(&self) -> Result<usize, String>;
+    }
+
+    #[async_trait::async_trait(?Send)]
+    impl AsyncTrait for Ctx {
+        async fn foo(&self) -> Result<usize, String> {
+            Ok(source(self).await + self.0)
+        }
+    }
+);
+
+fn calling_async_trait_policy(ctx: Arc<Context>) -> Result<()> {
+    let sources = Vec::from_iter(ctx.marked_nodes(Identifier::new_intern("source")));
+    let sinks = Vec::from_iter(ctx.marked_nodes(Identifier::new_intern("sink")));
+    assert_error!(ctx, !sources.is_empty());
+    assert_error!(ctx, !sinks.is_empty());
+    assert_error!(
+        ctx,
+        ctx.any_flows(&sources, &sinks, EdgeSelection::Data)
+            .is_some()
+    );
+    Ok(())
+}
+
+/// Turns out flowistry can actually handle calling async functions from
+/// `async_trait` as well. So here we test that that works.
+#[test]
+fn support_calling_async_trait_0_1_53() -> Result<()> {
+    let mut test = Test::new(CALLING_ASYNC_TRAIT_CODE)?;
+    test.with_dep(["async-trait@=0.1.53"]);
+    test.run(calling_async_trait_policy)
 }
