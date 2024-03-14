cceb843e3d4771d436f3c581c41f215acd12d6db
Justus Adam <justbldr@amazon.com>
2024-03-14 20:53:27 +0000

    Edit error messages for lemmy, add silent option

--- props/lemmy/src/main.rs
+++ props/lemmy/src/main.rs
@@ -22,97 +22,118 @@ macro_rules! marker {
 
 pub struct CommunityProp {
     cx: Arc<PolicyContext>,
+    args: &'static Arguments,
 }
 
 pub struct InstanceProp {
     cx: Arc<PolicyContext>,
+    args: &'static Arguments,
 }
 
 impl CommunityProp {
-    pub fn new(cx: Arc<PolicyContext>) -> Self {
-        CommunityProp { cx }
+    fn new(cx: Arc<PolicyContext>, args: &'static Arguments) -> Self {
+        CommunityProp { cx, args }
     }
 
-    pub fn check(&mut self) -> Result<()> {
+    fn check(&mut self) -> Result<()> {
         let mut community_writes = self.cx.marked_nodes(marker!(db_community_write));
         let mut delete_checks = self.cx.marked_nodes(marker!(community_delete_check));
         let mut ban_checks = self.cx.marked_nodes(marker!(community_ban_check));
 
-        let ok = community_writes.all(|write|
+        let ok = community_writes.all(|write| {
             delete_checks.any(|dc| self.cx.flows_to(dc, write, EdgeSelection::Both))
-            &&
-            ban_checks.any(|bc| self.cx.flows_to(bc, write, EdgeSelection::Both))
-        );
-            
-        assert_error!(
-            self.cx,
-            ok,
-            "Unauthorized community write"
-        );
+                && ban_checks.any(|bc| self.cx.flows_to(bc, write, EdgeSelection::Both))
+        });
+
+        assert_error!(self.cx, ok, "Unauthorized community write");
 
         Ok(())
     }
 }
 
 impl InstanceProp {
-    pub fn new(cx: Arc<PolicyContext>) -> Self {
-        InstanceProp { cx }
+    fn new(cx: Arc<PolicyContext>, args: &'static Arguments) -> Self {
+        InstanceProp { cx, args }
     }
 
-    pub fn check(&mut self) -> Result<()> {
-        let mut accesses = self.cx.marked_nodes(marker!(db_access)).filter(|n| !self.cx.has_marker(marker!(db_user_read), *n));
+    fn check(&mut self) -> Result<()> {
+        let accesses = self
+            .cx
+            .marked_nodes(marker!(db_access))
+            .filter(|n| !self.cx.has_marker(marker!(db_user_read), *n));
         let mut delete_checks = self.cx.marked_nodes(marker!(instance_delete_check));
         let mut ban_checks = self.cx.marked_nodes(marker!(instance_ban_check));
 
-        let ok = accesses.all(|access| {
-            delete_checks.any(|dc| self.cx.flows_to(dc, access, EdgeSelection::Both))
-            && 
-            ban_checks.any(|bc| self.cx.flows_to(bc, access, EdgeSelection::Both))
-        });
+        let mut del_checks_found = true;
+        let mut ban_checks_found = true;
 
-        if !ok {
-            let mut err = self.cx.struct_help(loc!("No auth check authorizing sink"));
+        for access in accesses {
+            if !delete_checks.any(|dc| self.cx.flows_to(dc, access, EdgeSelection::Both)) {
+                self.cx
+                    .node_error(access, "No delete check found for this access");
+                del_checks_found = false;
+            }
+            if !ban_checks.any(|bc| self.cx.flows_to(bc, access, EdgeSelection::Both)) {
+                self.cx
+                    .node_error(access, "No ban check found for this access");
+                ban_checks_found = false;
+            }
+        }
 
-            let accesses = self.cx.marked_nodes(marker!(db_access)).filter(|n| !self.cx.has_marker(marker!(db_user_read), *n));
-            let delete_checks = self.cx.marked_nodes(marker!(instance_delete_check));
-            let ban_checks = self.cx.marked_nodes(marker!(instance_ban_check));
+        if !del_checks_found && !self.args.quiet {
+            let mut delete_checks = self
+                .cx
+                .marked_nodes(marker!(instance_delete_check))
+                .peekable();
 
-            for access in accesses {
-                err.with_node_note(access, "This is a sink");
+            if delete_checks.peek().is_none() {
+                self.cx.warning("No delete checks were found");
             }
 
             for check in delete_checks {
-                err.with_node_note(check, "This is a delete check");
+                let mut help = self
+                    .cx
+                    .struct_node_help(check, "This is an elibigle delete check");
 
-                let influencees : Vec<GlobalNode> = self.cx.influencees(check, EdgeSelection::Both).collect();
+                let influencees: Vec<GlobalNode> =
+                    self.cx.influencees(check, EdgeSelection::Both).collect();
                 dbg!("There are {} influencees\n", influencees.len());
                 for influencee in influencees {
                     // NOTE: problem is that every influencee of check_user_valid is just itself
                     // so it doesn't influence the database access
-                    if influencee.controller_id() == check.controller_id() { continue };
-                    err.with_node_note(check, "This is an influencee of the delete check");
+                    if influencee.controller_id() == check.controller_id() {
+                        continue;
+                    };
+                    help.with_node_note(check, "This is an influencee of the delete check");
                 }
+                help.emit();
+            }
+        }
+
+        if !ban_checks_found && !self.args.quiet {
+            let mut ban_checks = self.cx.marked_nodes(marker!(instance_ban_check)).peekable();
+
+            if ban_checks.peek().is_none() {
+                self.cx.warning("No ban checks were found");
             }
 
             for check in ban_checks {
-                err.with_node_note(check, "This is a ban check");
+                let mut help = self
+                    .cx
+                    .struct_node_help(check, "This is an eligible ban check");
 
-                let influencees : Vec<GlobalNode> = self.cx.influencees(check, EdgeSelection::Both).collect();
+                let influencees: Vec<GlobalNode> =
+                    self.cx.influencees(check, EdgeSelection::Both).collect();
                 dbg!("There are {} influencees\n", influencees.len());
                 for influencee in influencees {
-                    if influencee.controller_id() == check.controller_id() { continue };
-                    err.with_node_note(check, "This is an influencee of the ban check");
+                    if influencee.controller_id() == check.controller_id() {
+                        continue;
+                    };
+                    help.with_node_note(check, "This is an influencee of the ban check");
                 }
+                help.emit();
             }
-
-            err.emit();
         }
-
-        assert_error!(
-            self.cx,
-            ok,
-            "Unauthorized instance db access"
-        );
 
         Ok(())
     }
@@ -125,13 +146,13 @@ enum Prop {
 }
 
 impl Prop {
-    fn run(self, cx: Arc<Context>) -> anyhow::Result<()> {
+    fn run(self, cx: Arc<Context>, args: &'static Arguments) -> anyhow::Result<()> {
         match self {
             Self::Community => cx.named_policy(Identifier::new_intern("Community Policy"), |cx| {
-                CommunityProp::new(cx.clone()).check()
+                CommunityProp::new(cx.clone(), args).check()
             }),
             Self::Instance => cx.named_policy(Identifier::new_intern("Instance Policy"), |cx| {
-                InstanceProp::new(cx.clone()).check()
+                InstanceProp::new(cx.clone(), args).check()
             }),
         }
     }
@@ -145,6 +166,8 @@ struct Arguments {
     /// Property selection. If none are selected all are run
     #[clap(long)]
     prop: Vec<Prop>,
+    #[clap(long, short)]
+    quiet: bool,
     #[clap(last = true)]
     extra_args: Vec<String>,
 }
@@ -189,7 +212,7 @@ fn main() -> anyhow::Result<()> {
         } else {
             args.prop.as_slice()
         } {
-            p.run(cx.clone())?;
+            p.run(cx.clone(), args)?;
         }
 
         anyhow::Ok(())
