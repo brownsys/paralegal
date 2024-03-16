2519ef8b88893c7dd9abc2ebea88d9d937dada76
Justus Adam <dev@justus.science>
2024-03-16 17:13:55 -0400

    Test case for flowing to self

--- crates/paralegal-policy/tests/freedit.rs
+++ crates/paralegal-policy/tests/freedit.rs
@@ -37,6 +37,150 @@ fn return_markers_on_no_arg_functions() -> Result<()> {
 }
 
 #[test]
+fn flows_to_self() -> Result<()> {
+    let test = Test::new(stringify!(
+        #[paralegal::marker(self, arguments = [0])]
+        fn foo(arg: usize) {}
+
+        #[paralegal::marker(noinline)]
+        fn non_const() -> usize {
+            unimplemented!()
+        }
+
+        #[paralegal::analyze]
+        fn main() {
+            let x = non_const();
+            let _ = foo(x);
+        }
+
+        #[paralegal::marker(source, return)]
+        fn source() -> usize {
+            9
+        }
+
+        #[paralegal::marker(target, arguments = [0])]
+        fn target(t: usize) {}
+
+        #[paralegal::analyze]
+        fn main2() {
+            target(source())
+        }
+    ))?;
+
+    test.run(|ctx| {
+        let marked_self = ctx
+            .marked_nodes(Identifier::new_intern("self"))
+            .collect::<Box<[_]>>();
+
+        assert_error!(ctx, !marked_self.is_empty());
+
+        assert_error!(
+            ctx,
+            ctx.any_flows(&marked_self, &marked_self, EdgeSelection::Data)
+                .is_some()
+        );
+        // QUESTION: Should this also hold for control flow??
+
+        let marked_source = ctx
+            .marked_nodes(Identifier::new_intern("source"))
+            .collect::<Box<[_]>>();
+        let marked_target = ctx
+            .marked_nodes(Identifier::new_intern("target"))
+            .collect::<Box<[_]>>();
+
+        assert_error!(ctx, !marked_source.is_empty());
+        assert_error!(ctx, !marked_target.is_empty());
+
+        assert_error!(
+            ctx,
+            ctx.any_flows(&marked_source, &marked_target, EdgeSelection::Data)
+                .is_some()
+        );
+
+        Ok(())
+    })
+}
+
+#[test]
+fn simple_monomorphization() -> Result<()> {
+    let test = Test::new(stringify!(
+        struct Donator;
+        struct Receiver;
+
+        trait Tr {
+            fn source(&self) -> usize;
+            fn target<T>(&self, t: T);
+        }
+
+        impl Tr for Donator {
+            #[paralegal::marker(source, return)]
+            fn source(&self) -> usize { 0 }
+            fn target<T>(&self, t: T) {}
+        }
+
+        impl Tr for Receiver {
+            fn source(&self) -> usize { 0 }
+            #[paralegal::marker(target, arguments = [1])]
+            fn target<T>(&self, t: T) {}
+        }
+
+        #[paralegal::analyze]
+        fn connected() {
+            Receiver.target(Donator.source())
+        }
+
+        #[paralegal::analyze]
+        fn unconnected() {
+            Receiver.target(Donator.source())
+        }
+    ))?;
+    test.run(|ctx| {
+        ctx.controller_contexts().for_each(|ctx| {
+            let sources: Box<[_]> = ctx
+                .marked_nodes(Identifier::new_intern("source"))
+                .filter(|n| n.controller_id() == ctx.id())
+                .collect();
+            let targets: Box<[_]> = ctx
+                .marked_nodes(Identifier::new_intern("target"))
+                .filter(|n| n.controller_id() == ctx.id())
+                .collect();
+
+            let expect_connect = ctx.current().name.as_str() != "connected";
+
+            assert_error!(
+                ctx,
+                !expect_connect || !sources.is_empty(),
+                "Source presence. Expectation: {}",
+                expect_connect
+            );
+            assert_error!(
+                ctx,
+                !expect_connect || !targets.is_empty(),
+                "Target presence. Expectation: {}",
+                expect_connect
+            );
+            assert_error!(
+                ctx,
+                !expect_connect
+                    || ctx
+                        .any_flows(&sources, &targets, EdgeSelection::Data)
+                        .is_some(),
+                "Flow. Expectation: {}",
+                expect_connect
+            );
+            for &src in sources.iter() {
+                ctx.node_note(src, format!("This is a source {}", ctx.describe_node(src)));
+            }
+            for &src in targets.iter() {
+                ctx.node_note(src, format!("This is a target {}", ctx.describe_node(src)));
+            }
+        });
+        Ok(())
+    })
+}
+
+#[test]
+#[ignore = "Marker assignment in generic functions that need monomorphization are broken."]
 fn markers_on_generic_calls() -> Result<()> {
     let test = Test::new(stringify!(
         struct Donator;
@@ -64,11 +208,6 @@ fn markers_on_generic_calls() -> Result<()> {
         }
 
         #[paralegal::analyze]
-        fn non_monomophized_resolve() {
-            Receiver.target(Donator.source())
-        }
-
-        #[paralegal::analyze]
         fn has_connection() {
             connect(Donator, Receiver);
         }
@@ -90,7 +229,7 @@ fn markers_on_generic_calls() -> Result<()> {
                 .filter(|n| n.controller_id() == ctx.id())
                 .collect();
 
-            let expect_connect = ctx.current().name.as_str() != "has_no_connection";
+            let expect_connect = ctx.current().name.as_str() == "has_connection";
 
             assert_error!(
                 ctx,
