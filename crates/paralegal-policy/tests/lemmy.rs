404208a5fc3338a503f44ab9d134208ca8abe734
Justus Adam <justbldr@amazon.com>
2024-03-17 22:28:42 +0000

    Minimal test case for lemmy error

--- crates/paralegal-policy/tests/lemmy.rs
+++ crates/paralegal-policy/tests/lemmy.rs
@@ -4,7 +4,7 @@ use std::sync::Arc;
 
 use helpers::{Result, Test};
 use paralegal_policy::{assert_error, assert_warning, Context, Diagnostics, EdgeSelection};
-use paralegal_spdg::Identifier;
+use paralegal_spdg::{GlobalNode, Identifier};
 
 const ASYNC_TRAIT_CODE: &str = stringify!(
     pub struct SaveComment {
@@ -14,14 +14,14 @@ const ASYNC_TRAIT_CODE: &str = stringify!(
     pub trait Perform {
         type Response;
 
-        async fn perform(&self) -> Result<Self::Response, String>;
+        async fn perform(&) -> Result<::Response, String>;
     }
 
     #[async_trait::async_trait(?Send)]
     impl Perform for SaveComment {
         type Response = ();
         #[paralegal::analyze]
-        async fn perform(&self) -> Result<(), String> {
+        async fn perform(&) -> Result<(), String> {
             save(create().await).await;
             Ok(())
         }
@@ -92,13 +92,13 @@ const CALLING_ASYNC_TRAIT_CODE: &str = stringify!(
 
     #[async_trait::async_trait(?Send)]
     trait AsyncTrait {
-        async fn foo(&self) -> Result<usize, String>;
+        async fn foo(&) -> Result<usize, String>;
     }
 
     #[async_trait::async_trait(?Send)]
     impl AsyncTrait for Ctx {
-        async fn foo(&self) -> Result<usize, String> {
-            Ok(source(self).await + self.0)
+        async fn foo(&) -> Result<usize, String> {
+            Ok(source().await + .0)
         }
     }
 );
@@ -123,4 +123,229 @@ fn support_calling_async_trait_0_1_53() -> Result<()> {
     let mut test = Test::new(CALLING_ASYNC_TRAIT_CODE)?;
     test.with_dep(["async-trait@=0.1.53"]);
     test.run(calling_async_trait_policy)
+}
+
+#[test]
+fn transitive_control_flow() -> Result<()> {
+    let test = Test::new(stringify!(
+        use std::future::Future;
+        use std::sync::Arc;
+
+        pub struct LemmyError {
+            inner: String
+        }
+
+        impl LemmyError {
+            fn from_message(s: &str) -> Self {
+                Self {
+                    inner: s.to_owned()
+                }
+            }
+        }
+
+        pub struct PgConnection;
+
+        #[derive(Clone)]
+        pub struct DbPool;
+
+        impl DbPool {
+            pub fn get(&self) -> Result<Arc<PgConnection>, LemmyError> {
+                Ok(Arc::new(PgConnection))
+            }
+        }
+
+        pub fn block<F, R>(f: F) -> impl Future<Output = Result<R, LemmyError>>
+        where
+            F: FnOnce() -> R + Send + 'static,
+            R: Send + 'static, {
+                std::future::ready(Ok(f()))
+            }
+
+        pub async fn blocking<F, T>(pool: &DbPool, f: F) -> Result<T, LemmyError>
+        where
+          F: FnOnce(&PgConnection) -> T + Send + 'static,
+          T: Send + 'static,
+        {
+          let pool = pool.clone();
+          let res = block(move || {
+            let conn = pool.get()?;
+            let res = (f)(&conn);
+            Ok(res) as Result<T, LemmyError>
+          })
+          .await?;
+
+          res
+        }
+
+        #[paralegal::marker(db_access, return)]
+        pub fn apply_label_read<T>(t: T) -> T { t }
+        #[paralegal::marker(instance_ban_check, return)]
+        pub fn apply_label_banned<T>(t: T) -> T { t }
+        #[paralegal::marker(instance_delete_check, return)]
+        pub fn apply_label_deleted<T>(t: T) -> T { t }
+        #[paralegal::marker(db_user_read, return)]
+        pub fn apply_label_user_read<T>(t: T) -> T { t }
+
+        pub struct GetUnreadRegistrationApplicationCount {
+            user_id: usize
+        }
+
+        pub struct LemmyContext {
+            pool: DbPool
+        }
+
+        impl LemmyContext {
+            pub fn pool(&self) -> &DbPool {
+                &self.pool
+            }
+        }
+
+        pub struct LocalUserView {
+            person: Person
+        }
+
+        pub struct Person {
+            banned: bool,
+            deleted: bool,
+        }
+
+        impl LocalUserView {
+            pub fn read(conn: &PgConnection, id: usize) -> Result<Self, LemmyError> {
+                Ok(LocalUserView {
+                    person: Person {
+                        banned: false,
+                        deleted: false,
+                    }
+                })
+            }
+        }
+
+        pub struct Site {
+            require_email_verification: bool
+        }
+
+        impl Site {
+            pub fn read_local_site(conn: &PgConnection) -> Result<Self, LemmyError> {
+                Ok(Site{ require_email_verification: true})
+            }
+        }
+        pub struct RegistrationApplicationView {
+
+        }
+
+        impl RegistrationApplicationView {
+            pub fn get_unread_count(conn: &PgConnection, _: bool) -> Result<usize, LemmyError> {
+                Ok(0)
+            }
+        }
+
+        #[paralegal::analyze]
+        async fn perform(
+            data: &GetUnreadRegistrationApplicationCount,
+            context: &LemmyContext,
+        ) -> Result<(), LemmyError> {
+            let pool = context.pool();
+            let local_user_id = data.user_id;
+            let local_user_view = apply_label_user_read(
+                blocking(pool, move |conn| LocalUserView::read(conn, local_user_id)).await??,
+            );
+
+            // Check for a site ban
+            if apply_label_banned(local_user_view.person.banned) {
+                return Err(LemmyError::from_message("site_ban"));
+            }
+
+            // Check for user deletion
+            if apply_label_deleted(local_user_view.person.deleted) {
+                return Err(LemmyError::from_message("deleted"));
+            }
+
+            let verified_email_only =
+                apply_label_read(blocking(context.pool(), Site::read_local_site).await??)
+                    .require_email_verification;
+
+            let registration_applications = apply_label_read(
+                blocking(context.pool(), move |conn| {
+                    RegistrationApplicationView::get_unread_count(conn, verified_email_only)
+                })
+                .await??,
+            );
+
+            Ok(())
+        }
+    ))?;
+
+    let instance_delete = Identifier::new_intern("instance_delete_check");
+    let instance_ban = Identifier::new_intern("instance_ban_check");
+
+    test.run(|ctx| {
+        let accesses = ctx
+            .marked_nodes(Identifier::new_intern("db_access"))
+            .filter(|n| !ctx.has_marker(Identifier::new_intern("db_user_read"), *n));
+        let mut delete_checks = ctx.marked_nodes(instance_delete);
+        let mut ban_checks = ctx.marked_nodes(instance_ban);
+
+        let mut del_checks_found = true;
+        let mut ban_checks_found = true;
+
+        for access in accesses {
+            if !delete_checks.any(|dc| ctx.flows_to(dc, access, EdgeSelection::Both)) {
+                ctx.node_error(access, "No delete check found for this access");
+                del_checks_found = false;
+            }
+            if !ban_checks.any(|bc| ctx.flows_to(bc, access, EdgeSelection::Both)) {
+                ctx.node_error(access, "No ban check found for this access");
+                ban_checks_found = false;
+            }
+        }
+
+        if !del_checks_found {
+            let mut delete_checks = ctx.marked_nodes(instance_delete).peekable();
+
+            if delete_checks.peek().is_none() {
+                ctx.warning("No delete checks were found");
+            }
+
+            for check in delete_checks {
+                let mut help = ctx.struct_node_help(check, "This is an elibigle delete check");
+
+                let influencees: Vec<GlobalNode> =
+                    ctx.influencees(check, EdgeSelection::Both).collect();
+                dbg!("There are {} influencees\n", influencees.len());
+                for influencee in influencees {
+                    // NOTE: problem is that every influencee of check_user_valid is just it
+                    // so it doesn't influence the database access
+                    if influencee.controller_id() == check.controller_id() {
+                        continue;
+                    };
+                    help.with_node_note(check, "This is an influencee of the delete check");
+                }
+                help.emit();
+            }
+        }
+
+        if !ban_checks_found {
+            let mut ban_checks = ctx.marked_nodes(instance_ban).peekable();
+
+            if ban_checks.peek().is_none() {
+                ctx.warning("No ban checks were found");
+            }
+
+            for check in ban_checks {
+                let mut help = ctx.struct_node_help(check, "This is an eligible ban check");
+
+                let influencees: Vec<GlobalNode> =
+                    ctx.influencees(check, EdgeSelection::Both).collect();
+                dbg!("There are {} influencees\n", influencees.len());
+                for influencee in influencees {
+                    if influencee.controller_id() == check.controller_id() {
+                        continue;
+                    };
+                    help.with_node_note(check, "This is an influencee of the ban check");
+                }
+                help.emit();
+            }
+        }
+        Ok(())
+    })
 }
