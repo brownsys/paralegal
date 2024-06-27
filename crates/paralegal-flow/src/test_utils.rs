59fca3515127dfe4041be27cbe1b9e6e74635c20
Justus Adam <justbldr@amazon.com>
2024-06-26 20:32:48 -0400

    Whoops

--- crates/paralegal-flow/src/test_utils.rs
+++ crates/paralegal-flow/src/test_utils.rs
@@ -232,20 +232,11 @@ impl InlineTestBuilder {
         args.setup_logging();
 
         rustc_utils::test_utils::CompileBuilder::new(&self.input)
-            .with_args(
-                [
-                    "--cfg",
-                    "paralegal",
-                    "-Zcrate-attr=feature(register_tool)",
-                    "-Zcrate-attr=register_tool(paralegal_flow)",
-                ]
-                .into_iter()
-                .map(ToOwned::to_owned),
-            )
-            .compile(move |result| {
-                let tcx = result.tcx;
-                let memo = crate::Callbacks::new(Box::leak(Box::new(args)));
-                let pdg = memo.run(tcx).unwrap();
+            .with_args(PARALEGAL_RUSTC_FLAGS.iter().copied().map(ToOwned::to_owned))
+            .compile(move |CompileResult { tcx }| {
+                let mut memo = Callbacks::new(Box::leak(Box::new(args)));
+                memo.persist_metadata = false;
+                let pdg = memo.run_compilation(tcx).unwrap().unwrap();
                 let graph = PreFrg::from_description(pdg);
                 let cref = graph.ctrl(&self.ctrl_name);
                 check(cref)
