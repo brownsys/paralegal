6d57693891654bb8efbf84d3b9e4d32bcf7ee99c
Justus Adam <justbldr@amazon.com>
2024-07-29 17:11:23 +0000

    Fixed the spans

--- crates/flowistry_pdg_construction/src/encoder.rs
+++ crates/flowistry_pdg_construction/src/encoder.rs
@@ -7,7 +7,7 @@
 //! unimplemented and will cause a crash if you try to stick an `AllocId` into
 //! the Paralegal artifact.
 
-use std::num::NonZeroU64;
+use std::{num::NonZeroU64, path::PathBuf};
 use std::path::Path;
 
 use rustc_const_eval::interpret::AllocId;
@@ -20,6 +20,7 @@ use rustc_serialize::{
     Decodable, Decoder, Encodable, Encoder,
 };
 
+use rustc_span::{BytePos, FileName, RealFileName, Span, SpanData, SyntaxContext, DUMMY_SP};
 use rustc_type_ir::{TyDecoder, TyEncoder};
 
 macro_rules! encoder_methods {
@@ -212,76 +213,115 @@ impl<'tcx, 'a> Decodable<ParalegalDecoder<'tcx, 'a>> for DefIndex {
     }
 }
 
-// const TAG_PARTIAL_SPAN: u8 = 0;
-// const TAG_VALID_SPAN_FULL: u8 = 1;
+const TAG_PARTIAL_SPAN: u8 = 0;
+const TAG_VALID_SPAN_FULL: u8 = 1;
 
-// impl<'tcx> Encodable<ParalegalEncoder<'tcx>> for Span {
-//     fn encode(&self, s: &mut ParalegalEncoder<'tcx>) {
-//         self.data().encode(s)
-//     }
-// }
+impl<'tcx> Encodable<ParalegalEncoder<'tcx>> for Span {
+    fn encode(&self, s: &mut ParalegalEncoder<'tcx>) {
+        self.data().encode(s)
+    }
+}
 
-// impl<'tcx> Encodable<ParalegalEncoder<'tcx>> for SpanData {
-//     fn encode(&self, s: &mut ParalegalEncoder<'tcx>) {
-//         if self.is_dummy() {
-//             return TAG_PARTIAL_SPAN.encode(s);
-//         }
-//         let source_map = s.tcx.sess.source_map();
-//         let source_file = source_map.lookup_source_file(self.lo);
-//         if !source_file.contains(self.hi) {
-//             // Unfortunately, macro expansion still sometimes generates Spans
-//             // that malformed in this way.
-//             return TAG_PARTIAL_SPAN.encode(s);
-//         }
-//         TAG_VALID_SPAN_FULL.encode(s);
-//         StableSourceFileId::new(&source_file).encode(s);
+impl<'tcx> Encodable<ParalegalEncoder<'tcx>> for SpanData {
+    fn encode(&self, s: &mut ParalegalEncoder<'tcx>) {
+        if self.is_dummy() {
+            return TAG_PARTIAL_SPAN.encode(s);
+        }
+        let source_map = s.tcx.sess.source_map();
+        let source_file = source_map.lookup_source_file(self.lo);
+        if !source_file.contains(self.hi) {
+            // Unfortunately, macro expansion still sometimes generates Spans
+            // that malformed in this way.
+            return TAG_PARTIAL_SPAN.encode(s);
+        }
+        TAG_VALID_SPAN_FULL.encode(s);
+        source_file.cnum.encode(s);
+        let mut name = source_file.name.clone();
+        if let FileName::Real(RealFileName::Remapped { local_path, .. }) = &mut name {
+            local_path.take();
+        }
+        name.encode(s);
 
-//         let lo = self.lo - source_file.start_pos;
-//         let len = self.hi - self.lo;
-//         lo.encode(s);
-//         len.encode(s);
-//     }
-// }
+        let lo = self.lo - source_file.start_pos;
+        let len = self.hi - self.lo;
+        lo.encode(s);
+        len.encode(s);
+    }
+}
 
-// impl<'tcx> Encodable<ParalegalEncoder<'tcx>> for SyntaxContext {
-//     fn encode(&self, _: &mut ParalegalEncoder<'tcx>) {}
-// }
+impl<'tcx> Encodable<ParalegalEncoder<'tcx>> for SyntaxContext {
+    fn encode(&self, _: &mut ParalegalEncoder<'tcx>) {}
+}
 
-// impl<'tcx, 'a> Decodable<ParalegalDecoder<'tcx, 'a>> for Span {
-//     fn decode(d: &mut ParalegalDecoder<'tcx, 'a>) -> Self {
-//         SpanData::decode(d).span()
-//     }
-// }
+impl<'tcx, 'a> Decodable<ParalegalDecoder<'tcx, 'a>> for Span {
+    fn decode(d: &mut ParalegalDecoder<'tcx, 'a>) -> Self {
+        SpanData::decode(d).span()
+    }
+}
 
-// impl<'tcx, 'a> Decodable<ParalegalDecoder<'tcx, 'a>> for SpanData {
-//     fn decode(d: &mut ParalegalDecoder<'tcx, 'a>) -> Self {
-//         let ctxt = SyntaxContext::decode(d);
-//         let tag = u8::decode(d);
-//         if tag == TAG_PARTIAL_SPAN {
-//             return DUMMY_SP.with_ctxt(ctxt).data();
-//         }
-//         debug_assert_eq!(tag, TAG_VALID_SPAN_FULL);
-//         let id = StableSourceFileId::decode(d);
-//         let source_file = d
-//             .tcx
-//             .sess
-//             .source_map()
-//             .source_file_by_stable_id(id)
-//             .unwrap();
-//         let lo = BytePos::decode(d);
-//         let len = BytePos::decode(d);
-//         let hi = lo + len;
-//         SpanData {
-//             lo: source_file.start_pos + lo,
-//             hi: source_file.start_pos + hi,
-//             ctxt,
-//             parent: None,
-//         }
-//     }
-// }
+fn path_in_real_path(r: &RealFileName) -> &PathBuf {
+    match r {
+        RealFileName::LocalPath(p) => p,
+        RealFileName::Remapped { virtual_name, .. } => virtual_name,
+    }
+}
 
-// impl<'tcx, 'a> Decodable<ParalegalDecoder<'tcx, 'a>> for SyntaxContext {
-//     fn decode(_: &mut ParalegalDecoder<'tcx, 'a>) -> Self {
-//         SyntaxContext::root()
-//     }
-// }
+impl<'tcx, 'a> Decodable<ParalegalDecoder<'tcx, 'a>> for SpanData {
+    fn decode(d: &mut ParalegalDecoder<'tcx, 'a>) -> Self {
+        let ctxt = SyntaxContext::decode(d);
+        let tag = u8::decode(d);
+        if tag == TAG_PARTIAL_SPAN {
+            return DUMMY_SP.with_ctxt(ctxt).data();
+        } 
+        debug_assert_eq!(tag, TAG_VALID_SPAN_FULL);
+        let crate_num = CrateNum::decode(d);
+        let file_name = FileName::decode(d);
+        let source_map = d
+            .tcx
+            .sess
+            .source_map();
+        let matching_source_files = source_map
+            .files()
+            .iter()
+            .filter(|f| {
+                f.cnum == crate_num && (file_name == f.name || matches!((&file_name, &f.name), (FileName::Real(r), FileName::Real(other)) if {
+                    let before = path_in_real_path(r);
+                    let after = path_in_real_path(other);
+                    after.ends_with(before)
+                })) 
+            })
+            .cloned()
+            .collect::<Box<[_]>>();
+        let source_file = match matching_source_files.as_ref() {
+            [sf] => {
+                println!("Approximate match succeeded {file_name:?} matches {:?}", sf.name);
+                sf.clone()
+            },
+            [] => {
+                match &file_name {
+                    FileName::Real(RealFileName::LocalPath(local)) if source_map.file_exists(local) => source_map.load_file(local).unwrap(),
+                    _ => panic!("Could not load file {}", file_name.prefer_local()),
+                }
+            },
+            other => {
+                let names = other.iter().map(|f| &f.name).collect::<Vec<_>>();
+                panic!("Too many matching file names for {file_name:?}: {names:?}")
+            }
+        };
+        let lo = BytePos::decode(d);
+        let len = BytePos::decode(d);
+        let hi = lo + len;
+        SpanData {
+            lo: source_file.start_pos + lo,
+            hi: source_file.start_pos + hi,
+            ctxt,
+            parent: None,
+        }
+    }
+}
+
+impl<'tcx, 'a> Decodable<ParalegalDecoder<'tcx, 'a>> for SyntaxContext {
+    fn decode(_: &mut ParalegalDecoder<'tcx, 'a>) -> Self {
+        SyntaxContext::root()
+    }
+}
