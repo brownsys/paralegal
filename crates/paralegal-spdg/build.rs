6192b59f3926244a5c71bd8b26b4da4492c02ab3
Justus Adam <dev@justus.science>
2026-04-21 12:40:36 -0400

    rm indexical

--- crates/paralegal-spdg/build.rs
+++ crates/paralegal-spdg/build.rs
@@ -1,25 +1,11 @@
 use anyhow::Context;
 use anyhow::Result;
+use paralegal_non_rustc_utils::linux_workaround_for_llvm_lib;
 use std::collections::hash_map::DefaultHasher;
-use std::env;
 use std::fs;
 use std::hash::{Hash, Hasher};
-use std::path::{Path, PathBuf};
+use std::path::Path;
 use std::time::SystemTime;
-
-fn rustup_toolchain_path() -> PathBuf {
-    let rustup_home = env::var("RUSTUP_HOME").unwrap();
-    let rustup_tc = env::var("RUSTUP_TOOLCHAIN").unwrap();
-    [&rustup_home, "toolchains", &rustup_tc]
-        .into_iter()
-        .collect()
-}
-
-fn get_rustup_lib_path() -> PathBuf {
-    let mut rustup_lib = rustup_toolchain_path();
-    rustup_lib.push("lib");
-    rustup_lib
-}
 
 /// Helper for calculating a hash of all (modification dates of) Rust files in this crate.
 fn visit_dirs(dir: &Path, hasher: &mut DefaultHasher) -> Result<()> {
@@ -62,9 +48,5 @@ fn main() {
     // Emit the hash as an environment variable
     println!("cargo:rustc-env=SER_MAGIC={:0}", magic);
 
-    // Original linux-specific code
-    if cfg!(target_os = "linux") {
-        let rustup_lib = get_rustup_lib_path();
-        println!("cargo:rustc-link-search=native={}", rustup_lib.display());
-    }
+    linux_workaround_for_llvm_lib();
 }
